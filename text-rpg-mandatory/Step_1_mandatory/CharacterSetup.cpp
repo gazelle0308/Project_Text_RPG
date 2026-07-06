860041fd2e9b75d6c9310bf6b276c4744ce8643b
gazelle0308 <zowo0056@gmail.com>
2026-07-06 16:14:42 +0900

    Implement Step1 Character Setup

---
+++ text-rpg-mandatory/Step_1_mandatory/CharacterSetup.cpp
@@ -0,0 +1,44 @@
+#include "CharacterSetup.h"
+
+int main()
+{
+
+    const int SIZE = 4;
+
+    string name;
+    int stat[SIZE] = {0};
+
+    SetupStatus(name, stat);
+    PrintStatus(name, stat);
+
+    return 0;
+
+}
+
+void SetupStatus(string& InsertName, int InsertStat[])
+{
+
+    cout << "=================================================" << endl;
+    cout << "[Dungeon Escape RPG]" << endl;
+    cout << "=================================================" << endl;
+    cout << "Input your chracter name: ";
+    getline(cin, InsertName);
+    cout << "Input your character's HP and MP: ";
+    cin >> InsertStat[0] >> InsertStat[1];
+    cout << "Input your character's attack and defense: ";
+    cin >> InsertStat[2] >> InsertStat[3];
+    cout << "=================================================" << endl;
+    //stat[0] = HP, stat[1] = MP, stat[2] = 공격력, stat[3] = 방어력
+}
+
+void PrintStatus(const string PrintName, const int PrintStat[])
+{
+
+    cout << "================================" << endl;
+    cout << PrintName << "'s stats:" << endl;
+    cout << "================================" << endl;
+    cout << "HP : " << PrintStat[0] << " MP : " << PrintStat[1] << endl;
+    cout << "Attack : " << PrintStat[2] << " Defense : " << PrintStat[3] << endl;
+    cout << "================================" << endl;
+
+}
---
+++ text-rpg-mandatory/Step_1_mandatory/CharacterSetup.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void SetupStatus(string& InsertName, int InsertStat[]);
+void PrintStatus(const string PrintName, const int PrintStat[]);
